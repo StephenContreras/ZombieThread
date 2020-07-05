3d623735dee9b7172f1642a7971bbe63da25fc19
Stephen Contreras <stephenjcontreras@gmail.com>
2020-07-05 05:12:30 -0500

    rm footer

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.scss';
 import Header from './Header'
 import DescNPlayer from './DescNPlayer'
-import Footer from './Footer'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <DescNPlayer/>
-      <Footer/>
     </div>
   );
 }
